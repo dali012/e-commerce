240298a6e7e007e73e1d73b2cf8916ba2b4b477a
dali012 <dali.jerbi97@gmail.com>
2022-09-07 07:59:52 +0100

    fix: depency removed

--- iphone-ecommerce/sanity-codegen.config.ts
+++
@@ -1,8 +0,0 @@
-import { SanityCodegenConfig } from "sanity-codegen";
-
-const config: SanityCodegenConfig = {
-  schemaPath: "./schemas/schema.js",
-  outputPath: "./schema.ts",
-};
-
-export default config;
